5bfe9b268cc388334bf2eb7f80d98c4f8762e901
ahmednoorzakaria <ahmednoor.zakaria@student.moringaschool.com>
2023-07-20 11:38:16 +0300

    Added usestate

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleCartToggle() {
+    setInCart((prevInCart) => !prevInCart);
+  }
+
+  const className = inCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={className}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleCartToggle}>
+        {inCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
 
 export default Item;
+
